0d0f1ba493e4cbf79e7d8432b7f10f28440eafc2
vokenboy <viliustam36@gmail.com>
2025-05-26 14:52:58 +0300

    card

--- client/src/components/ForecastCard.vue
+++ client/src/components/ForecastCard.vue
@@ -1,3 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// This component is a hardcoded example of a weather forecast card.
+// Later, you can replace hardcoded values with props for dynamic data.
+</script>
 
-<template></template>
+<template>
+    <div class="card" style="width: 18rem">
+        <header class="card-header">
+            <p class="card-header-title">Vilnius, LT</p>
+            <button
+                class="card-header-icon button is-small is-danger"
+                title="Remove"
+            >
+                &times;
+            </button>
+        </header>
+        <div class="card-content has-text-centered">
+            <img
+                src="https://openweathermap.org/img/wn/01d@2x.png"
+                alt="Clear sky"
+            />
+            <p>20°C | 60% Humidity | 5 m/s Wind</p>
+            <p>Pressure: 1013 hPa</p>
+            <p>Sunrise: 05:12 | Sunset: 20:45</p>
+        </div>
+    </div>
+</template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import ForecastCard from "../components/ForecastCard.vue";
 </script>
 <template>
     <input class="input" type="text" placeholder="Text input" />
     <button class="button is-primary">Add forecast</button>
+    <ForecastCard></ForecastCard>
 </template>
